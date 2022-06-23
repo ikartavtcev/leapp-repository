e3b9d10f69456da24d211c4fb855184446a1f68c
Roman Prilipskii <rprilipskii@cloudlinux.com>
2022-06-23 17:28:00 +0400

    Workaround actor not as unneeded as it seemed, return it

---
+++ repos/system_upgrade/cloudlinux/actors/clearpackageconflicts/actor.py
@@ -0,0 +1,35 @@
+import os
+import errno
+import shutil
+
+from leapp.actors import Actor
+from leapp.libraries.common.rpms import has_package
+from leapp.models import InstalledRPM
+from leapp.tags import DownloadPhaseTag, IPUWorkflowTag
+
+
+class ClearPackageConflicts(Actor):
+    """
+    Remove files manually to resolve conflicts between versions of packages to be upgraded.
+    """
+
+    name = 'clear_package_conflicts'
+    consumes = (InstalledRPM,)
+    produces = ()
+    tags = (DownloadPhaseTag.Before, IPUWorkflowTag)
+
+    def process(self):
+        problem_packages = ["alt-python37-six", "alt-python37-pytz"]
+        problem_packages_installed = any([has_package(InstalledRPM, pkg) for pkg in problem_packages])
+
+        if problem_packages_installed:
+            problem_dirs = [
+                "/opt/alt/python37/lib/python3.7/site-packages/six-1.15.0-py3.7.egg-info",
+                "/opt/alt/python37/lib/python3.7/site-packages/pytz-2017.2-py3.7.egg-info"]
+            for p_dir in problem_dirs:
+                try:
+                    if os.path.isdir(p_dir):
+                        shutil.rmtree(p_dir)
+                except OSError as e:
+                    if e.errno != errno.ENOENT:
+                        raise
