41984438b9a22cb2c4d89ab368a4ec944e3717b9
Roman Prilipskii <rprilipskii@cloudlinux.com>
2022-06-24 13:34:04 +0400

    Add the deployment script to utils

---
+++ utils/cl_elevate_prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
+yum install -y leapp-upgrade leapp-data-almalinux
+
+git clone -b cloudlinux --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository
+
+yes | mv /root/leapp-repository/repos/system_upgrade/common/libraries/config/version.py /etc/leapp/repos.d/system_upgrade/common/libraries/config/version.py
+yes | mv /root/leapp-repository/repos/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py /etc/leapp/repos.d/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py
+yes | mv /root/leapp-repository/repos/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py
+yes | cp -R /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.4 /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.6
+yes | cp -R /root/leapp-repository/repos/system_upgrade/cloudlinux /etc/leapp/repos.d/system_upgrade/system_upgrade/
+
+git clone -b cloudlinux --single-branch https://github.com/prilr/leapp-data.git /root/leapp-data
+rsync -a /root/leapp-data/files/cloudlinux/ /etc/leapp/files/
+
+rmmod floppy pata_acpi
+echo PermitRootLogin yes | tee -a /etc/ssh/sshd_config
+leapp answer --add --section remove_pam_pkcs11_module_check.confirm=True
+
+echo -e "\nYou now can run \"leapp upgrade\"!\n"
