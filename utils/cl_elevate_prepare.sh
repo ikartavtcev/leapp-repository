eeaed4b87810d19940d0c497e7f685bc30deb990
Roman Prilipskii <rprilipskii@cloudlinux.com>
2022-07-13 05:29:13 +0400

    Deployment script touch-up: base on cloudlinux branch

--- utils/cl_elevate_prepare.sh
+++ utils/cl_elevate_prepare.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
 yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
-yum install -y leapp-upgrade leapp-data-almalinux
+yum install -y leapp-upgrade leapp-data-almalinux git nano
 
-git clone -b 3rd_parties --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository
+git clone -b cloudlinux --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository-cloudlinux
+git clone -b 3rd_parties --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository-3rd_parties
 
-rsync -a /root/leapp-repository/repos/system_upgrade /etc/leapp/repos.d/system_upgrade
-/bin/cp -RT /root/leapp-repository/repos/system_upgrade/ /etc/leapp/repos.d/system_upgrade/
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/libraries/config/version.py /etc/leapp/repos.d/system_upgrade/common/libraries/config/version.py
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py /etc/leapp/repos.d/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/models/targetsystemtype.py /etc/leapp/repos.d/system_upgrade/common/models/targetsystemtype.py
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/scancustomrepofile/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/actor.py
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py
+yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/setuptargetrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/setuptargetrepos/actor.py
+
+mkdir /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos
+mkdir -p /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries
+mkdir /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping
+
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/checkenabledvendorrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos/actor.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/actor.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py /etc/leapp/repos.d/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/actor.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/libraries/repomaputils.py /etc/leapp/repos.d/system_upgrade/common/libraries/repomaputils.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/activevendorlist.py /etc/leapp/repos.d/system_upgrade/common/models/activevendorlist.py
+yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/repositoriesmap.py /etc/leapp/repos.d/system_upgrade/common/models/repositoriesmap.py
 
 yes | cp -R /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.4 /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.6
+yes | cp -R /root/leapp-repository-cloudlinux/repos/system_upgrade/cloudlinux /etc/leapp/repos.d/system_upgrade/cloudlinux
 
 git clone -b 3rd_parties --single-branch https://github.com/prilr/leapp-data.git /root/leapp-data
 rsync -a /root/leapp-data/files/cloudlinux/ /etc/leapp/files/
+
+LEAPP_DEVEL_USE_PERSISTENT_PACKAGE_CACHE=1
+# LEAPP_DEVEL_RPMS_ALL_SIGNED=1
+# LEAPP_DEVEL_TARGET_PRODUCT_TYPE=beta
 
 rmmod floppy pata_acpi btrfs
 echo PermitRootLogin yes | tee -a /etc/ssh/sshd_config
