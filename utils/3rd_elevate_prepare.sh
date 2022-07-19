77f5365d81a1c72b67e31bfbcdb6a6220f3dae3a
Roman Prilipskii <rprilipskii@cloudlinux.com>
2022-07-19 17:24:44 +0400

    Update deployment scripts

--- utils/3rd_elevate_prepare.sh
+++ utils/3rd_elevate_prepare.sh
@@ -9,16 +9,16 @@ mkdir /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos
 mkdir -p /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries
 mkdir /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping
 
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/checkenabledvendorrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py /etc/leapp/repos.d/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/libraries/repomaputils.py /etc/leapp/repos.d/system_upgrade/common/libraries/repomaputils.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/activevendorlist.py /etc/leapp/repos.d/system_upgrade/common/models/activevendorlist.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/repositoriesmap.py /etc/leapp/repos.d/system_upgrade/common/models/repositoriesmap.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/checkenabledvendorrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py /etc/leapp/repos.d/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/libraries/repomaputils.py /etc/leapp/repos.d/system_upgrade/common/libraries/repomaputils.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/activevendorlist.py /etc/leapp/repos.d/system_upgrade/common/models/activevendorlist.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/repositoriesmap.py /etc/leapp/repos.d/system_upgrade/common/models/repositoriesmap.py
 
 LEAPP_DEVEL_USE_PERSISTENT_PACKAGE_CACHE=1
 # LEAPP_DEVEL_TARGET_PRODUCT_TYPE=ga
--- utils/cl_elevate_prepare.sh
+++ utils/cl_elevate_prepare.sh
@@ -6,28 +6,30 @@ yum install -y leapp-upgrade leapp-data-almalinux git nano
 git clone -b cloudlinux --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository-cloudlinux
 git clone -b 3rd_parties --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository-3rd_parties
 
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/libraries/config/version.py /etc/leapp/repos.d/system_upgrade/common/libraries/config/version.py
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py /etc/leapp/repos.d/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/models/targetsystemtype.py /etc/leapp/repos.d/system_upgrade/common/models/targetsystemtype.py
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/scancustomrepofile/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/actor.py
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py
-yes | mv -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/setuptargetrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/setuptargetrepos/actor.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/libraries/config/version.py /etc/leapp/repos.d/system_upgrade/common/libraries/config/version.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py /etc/leapp/repos.d/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/models/targetsystemtype.py /etc/leapp/repos.d/system_upgrade/common/models/targetsystemtype.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/scancustomrepofile/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/actor.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/setuptargetrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/setuptargetrepos/actor.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/networkmanagerupdateconnections/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/networkmanagerupdateconnections/actor.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/actors/networkmanagerupdateconnections/tools/nm-update-client-ids.py /etc/leapp/repos.d/system_upgrade/common/actors/networkmanagerupdateconnections/tools/nm-update-client-ids.py
 
 mkdir /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos
 mkdir -p /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries
 mkdir /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping
 
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/checkenabledvendorrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py /etc/leapp/repos.d/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/libraries/repomaputils.py /etc/leapp/repos.d/system_upgrade/common/libraries/repomaputils.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/activevendorlist.py /etc/leapp/repos.d/system_upgrade/common/models/activevendorlist.py
-yes | mv -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/repositoriesmap.py /etc/leapp/repos.d/system_upgrade/common/models/repositoriesmap.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/checkenabledvendorrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/checkenabledvendorrepos/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py /etc/leapp/repos.d/system_upgrade/common/actors/peseventsscanner/libraries/peseventsscanner.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py /etc/leapp/repos.d/system_upgrade/common/actors/repositoriesmapping/libraries/repositoriesmapping.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py /etc/leapp/repos.d/system_upgrade/common/actors/scanvendorrepofiles/libraries/scanvendorrepofiles.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/vendorrepositoriesmapping/actor.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/libraries/repomaputils.py /etc/leapp/repos.d/system_upgrade/common/libraries/repomaputils.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/activevendorlist.py /etc/leapp/repos.d/system_upgrade/common/models/activevendorlist.py
+yes | cp -f /root/leapp-repository-3rd_parties/repos/system_upgrade/common/models/repositoriesmap.py /etc/leapp/repos.d/system_upgrade/common/models/repositoriesmap.py
 
 yes | cp -R /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.4 /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.6
 yes | cp -R /root/leapp-repository-cloudlinux/repos/system_upgrade/cloudlinux /etc/leapp/repos.d/system_upgrade/cloudlinux
