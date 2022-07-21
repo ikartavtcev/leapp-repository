89229f322ced738beb1781d7b1c22024341fe355
Roman Prilipskii <rprilipskii@cloudlinux.com>
2022-07-21 18:30:36 +0400

    Update deployment script

--- utils/cl_elevate_prepare.sh
+++ utils/cl_elevate_prepare.sh
@@ -3,19 +3,22 @@
 yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
 yum install -y leapp-upgrade leapp-data-almalinux
 
+rm -rf /root/leapp-repository
 git clone -b cloudlinux --single-branch https://github.com/prilr/leapp-repository.git /root/leapp-repository
 
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/libraries/config/version.py /etc/leapp/repos.d/system_upgrade/common/libraries/config/version.py
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py /etc/leapp/repos.d/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/models/targetsystemtype.py /etc/leapp/repos.d/system_upgrade/common/models/targetsystemtype.py
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/actors/scancustomrepofile/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/actor.py
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py
-yes | mv -f /root/leapp-repository/repos/system_upgrade/common/actors/setuptargetrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/setuptargetrepos/actor.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/libraries/config/version.py /etc/leapp/repos.d/system_upgrade/common/libraries/config/version.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py /etc/leapp/repos.d/system_upgrade/common/actors/ipuworkflowconfig/libraries/ipuworkflowconfig.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/redhatsignedrpmscanner/actor.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/models/targetsystemtype.py /etc/leapp/repos.d/system_upgrade/common/models/targetsystemtype.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/actors/scancustomrepofile/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/actor.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py /etc/leapp/repos.d/system_upgrade/common/actors/scancustomrepofile/libraries/scancustomrepofile.py
+yes | cp -f /root/leapp-repository/repos/system_upgrade/common/actors/setuptargetrepos/actor.py /etc/leapp/repos.d/system_upgrade/common/actors/setuptargetrepos/actor.py
+yes | cp -f /root/leapp-repository-cloudlinux/repos/system_upgrade/common/libraries/dnfplugin.py /etc/leapp/repos.d/system_upgrade/common/libraries/dnfplugin.py
 
 yes | cp -R /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.4 /etc/leapp/repos.d/system_upgrade/common/files/prod-certs/8.6
 yes | cp -R /root/leapp-repository/repos/system_upgrade/cloudlinux /etc/leapp/repos.d/system_upgrade/cloudlinux
 
+rm -rf /root/leapp-data
 git clone -b cloudlinux --single-branch https://github.com/prilr/leapp-data.git /root/leapp-data
 rsync -a /root/leapp-data/files/cloudlinux/ /etc/leapp/files/
 
