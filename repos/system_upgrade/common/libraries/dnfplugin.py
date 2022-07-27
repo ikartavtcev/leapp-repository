5b9de2ddfeb0454d3b81a99158930d3b0c44237e
Roman Prilipskii <rprilipskii@cloudlinux.com>
2022-07-27 11:54:01 +0400

    Fix the dnfplugin str encode errors

--- repos/system_upgrade/common/libraries/dnfplugin.py
+++ repos/system_upgrade/common/libraries/dnfplugin.py
@@ -4,6 +4,8 @@ import json
 import os
 import shutil
 
+import six
+
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries.common import dnfconfig, guards, mounting, overlaygen, rhsm, utils
 from leapp.libraries.common.config.version import get_target_major_version, get_source_major_version
@@ -190,10 +192,15 @@ def _transaction(context, stage, target_repoids, tasks, plugin_info, test=False,
                 message='Failed to execute dnf. Reason: {}'.format(str(e))
             )
         except CalledProcessError as e:
+            if six.PY2:
+                e.stdout = e.stdout.encode('utf-8', 'xmlcharrefreplace')
+                e.stderr = e.stdout.encode('utf-8', 'xmlcharrefreplace')
+
             api.current_logger().error('DNF execution failed: ')
             raise StopActorExecutionError(
                 message='DNF execution failed with non zero exit code.\nSTDOUT:\n{stdout}\nSTDERR:\n{stderr}'.format(
-                    stdout=e.stdout, stderr=e.stderr)
+                    stdout=e.stdout, stderr=e.stderr
+                )
             )
         finally:
             if stage == 'check':
@@ -327,14 +334,17 @@ def perform_transaction_check(target_userspace_info, used_repos, tasks, xfs_info
             )
 
 
-def prepare_channel(context):
+def _prepare_channel(context):
     import subprocess
-    up2date_config = '/etc/sysconfig/rhn/up2date'
-    channel_check = ['/usr/sbin/rhn-channel', '-l']
-    channel_reg = ['/usr/sbin/rhnreg_ks', '--force', '--serverUrl=https://xmlrpc.cln-staging.cloudlinux.com/XMLRPC/', '--activationkey=IPL']
-    update_release = ['yum', 'update', '-y', 'cloudlinux-release']
-    subprocess.call(channel_reg)
-    subprocess.call(update_release)
+    try:
+        up2date_config = '/etc/sysconfig/rhn/up2date'
+        channel_check = ['/usr/sbin/rhn-channel', '-l']
+        channel_reg = ['/usr/sbin/rhnreg_ks', '--force', '--serverUrl=https://xmlrpc.cln-staging.cloudlinux.com/XMLRPC/', '--activationkey=IPL']
+        update_release = ['yum', 'update', '-y', 'cloudlinux-release']
+        subprocess.call(channel_reg)
+        subprocess.call(update_release)
+    except OSError as e:
+        api.current_logger().error('Could not call RHN command: Message: %s', str(e), exc_info=True)
 
 
 def perform_rpm_download(target_userspace_info, used_repos, tasks, xfs_info, storage_info, plugin_info, on_aws=False):
@@ -347,7 +357,7 @@ def perform_rpm_download(target_userspace_info, used_repos, tasks, xfs_info, sto
         with overlaygen.create_source_overlay(mounts_dir=userspace_info.mounts, scratch_dir=userspace_info.scratch,
                                               xfs_info=xfs_info, storage_info=storage_info,
                                               mount_target=os.path.join(context.base_dir, 'installroot')) as overlay:
-            prepare_channel(context)
+            _prepare_channel(context)
             _apply_yum_workaround(overlay.nspawn())
             dnfconfig.exclude_leapp_rpms(context)
             _transaction(
