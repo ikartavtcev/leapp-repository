a2fbff2f4a9db0b40e7f4bfca6b6d7201cf81a74
Aleksey Petryankin <apetryankin@cloudlinux.com>
2022-08-10 16:26:47 +0300

    run cagefsctl --force-update automatically in elevate

---
+++ repos/system_upgrade/cloudlinux/actors/updatecagefs/actor.py
@@ -0,0 +1,34 @@
+import os
+
+from leapp.actors import Actor
+from leapp.libraries.stdlib import run, CalledProcessError
+from leapp.reporting import Report, create_report
+from leapp import reporting
+from leapp.tags import FirstBootPhaseTag, IPUWorkflowTag
+
+
+class UpdateCagefs(Actor):
+    """
+    Force update of cagefs.
+
+    cagefs should reflect massive changes in system made in previous phases
+    """
+
+    name = 'update_cagefs'
+    consumes = ()
+    produces = ()
+    tags = (FirstBootPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        if os.path.exists('/usr/sbin/cagefsctl'):
+            try:
+                run(['/usr/sbin/cagefsctl', '--force-update'], checked=True)
+                self.log.info('cagefs update was successful')
+            except CalledProcessError as e:
+                # cagefsctl prints errors in stdout
+                self.log.error(e.stdout)
+                self.log.error('Command "cagefsctl --force-update" finished with exit code {}, '
+                    'file system inside cagefs may by out-of-date.\n'
+                    'Check cagefsctl output above and /var/log/cagefs-update.log, '
+                    'rerun "cagefsctl --force-update" after fixing the issues.'.format(e.exit_code)
+                )
